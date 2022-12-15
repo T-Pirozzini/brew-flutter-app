800e923109b09a8ba9af863f0bee20241b22c6ce
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-12-14 17:44:17 -0800

    add custom fonts and styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey ninjas:'),
+  home: Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+    body: Center(
+      child: Text(
+          'Hello Ninjas',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('Click'),
+      backgroundColor: Colors.green[600],
+    ),
+  ),
 ));
+
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
