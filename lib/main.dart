4d34350c06671ccd542c6b03e58fb47807ab9e97
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-12-19 21:42:36 -0800

    hot key - quick access shortcuts

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,31 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green[600],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
+          Row(
+            children: [
+              Text('hello im a row'),
+              Text("so am i!"),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.redAccent,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('two'),
+                ),
+              ),
+            ],
+          ),
           Container(
             padding: EdgeInsets.all(20.0),
             color: Colors.cyan,
             child: Text('one'),
-          ),
-          Container(
-            padding: EdgeInsets.all(30.0),
-            color: Colors.redAccent,
-            child: Text('two'),
           ),
           Container(
             padding: EdgeInsets.all(40.0),
